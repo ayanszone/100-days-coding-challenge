71dd1b9407cf699a3343a70fd262d1b0a0ced6f5
ayanszone <sheikhayaan470@gmail.com>
2024-03-10 11:46:30 +0000

    Updated Repository

---
+++ Day-04/12.greetings/main.ts
@@ -0,0 +1,8 @@
+/*Question 12: Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. 
+The message should be the same for each person, but personalized with their name.*/
+let friendName = ["Ayaan,","Ahmed,","Sheikh,","Areez,"];
+let message = (` I'm thrilled to report that I've wrapped up 4 tasks.`);
+console.log(`Hello`,friendName[0]+message);
+console.log(`Hello`,friendName[1]+message);
+console.log(`Hello`,friendName[2]+message);
+console.log(`Hello`,friendName[3]+message);
